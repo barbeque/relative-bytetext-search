38f277ec5c45858f392f2ae50f6c950021fa52d8
mike stedman <barbeque@users.noreply.github.com>
2018-02-19 09:30:45 -0700

    Found stuff, but maybe the wrong stuff

--- hunt.py
+++ hunt.py
@@ -14,13 +14,24 @@ def guess(buf, char_offset, search_string):
     return True
 
 def hunt(filename, search_string):
+    attempts = 0
     print('Looking for ' + search_string + ' in ' + filename + '\n')
     with open(filename, "rb") as f:
         for char_offset in range(-256,256):
-            buf = f.read(len(search_string))
-            if not buf: break
-            if guess(buf, char_offset, search_string):
-                print('We found the offset!')
-                break
+            while True:
+                buf = f.read(len(search_string))
+                if not buf:
+                    print 'Done with char_offset = %d' % char_offset
+                    break
+                attempts += 1
+                if guess(buf, char_offset, search_string):
+                    print('We found the offset!')
+                    break
+                    # reset the file pointer for another read
+            f.seek(0)
+    print('Total attempts: %d' % attempts)
+
+# FIXME: do this in a 'signal' approach (relative offset from c[i+1] to c[1])
+# so it's faster
 
 if __name__ == '__main__': hunt(sys.argv[1], ' '.join(sys.argv[2:]))
